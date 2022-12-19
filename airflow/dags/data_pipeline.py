dc8688cd827396c5e7ac65bc057f6661742a9150
lianzy81 <lianzy@gmail.com>
2022-12-19 23:54:46 +0800

    updated data_pipeline.py to schedule hourly execution at 15 minutes past the hour starting from UTC 2022-12-22 00:00

--- airflow/dags/data_pipeline.py
+++ airflow/dags/data_pipeline.py
@@ -13,8 +13,8 @@ from datetime import datetime
 
 with DAG(
     dag_id="data_pipeline_dag",
-    start_date=datetime(2022, 12, 14),
-    schedule=None #"0 * * * *"
+    start_date=datetime(2022, 12, 22),
+    schedule="15 * * * *" #None
 ) as dag:
 
     start_task = EmptyOperator(
